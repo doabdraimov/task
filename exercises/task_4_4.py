b810c880583a38791f3f0b1e49a07508368c4526
doabdraimov <46672698+doabdraimov@users.noreply.github.com>
2019-01-28 17:53:59 +0600

    обновил комментария

--- exercises/task_4_4.py
+++ exercises/task_4_4.py
@@ -5,7 +5,8 @@ command2 = 'switchport trunk allowed vlan 1,3,100,200,300'
 
 set(command1.lstrip('switchport trunk allowed vlan ').split(',')) & set(command2.lstrip('switchport trunk allowed vlan ').split(',')) 
 
-или так, построчно, чуть понятней  
+#пояснения
+#или так, по строчно, чуть понятней  
 command1 = 'switchport trunk allowed vlan 1,3,10,20,30,100'
 command2 = 'switchport trunk allowed vlan 1,3,100,200,300'
 
@@ -18,12 +19,12 @@ a.intersection(b)
 
 
 ###########
-объясненя по строчно
-удалеяем левую часть строки
+#пояснения по строчно
+#удалеяем левую часть списка
 command1.lstrip('switchport trunk allowed vlan ') 
-разбиваем строку на части по разделителю (,)
+#разбиваем список на части по разделителю (,)
 command1.split(','))
-преобразуем строку в множества
+#преобразуем список в множества
 a=set(command1.lstrip('switchport trunk allowed vlan ').split(','))
-получаем пересения множества 
+#получаем пересения множества 
 a & b  или еще можно a.intersection(b)
