18372f31750f5d3f761161460ff04b9656177003
doabdraimov <46672698+doabdraimov@users.noreply.github.com>
2019-01-28 17:28:01 +0600

    add comment

--- exercises/task_4_4.py
+++ exercises/task_4_4.py
@@ -1,3 +1,11 @@
+все в одной строке 
+
+command1 = 'switchport trunk allowed vlan 1,3,10,20,30,100'
+command2 = 'switchport trunk allowed vlan 1,3,100,200,300'
+
+set(command1.lstrip('switchport trunk allowed vlan ').split(',')) & set(command2.lstrip('switchport trunk allowed vlan ').split(',')) 
+
+или так, построчно, чуть понятней  
 command1 = 'switchport trunk allowed vlan 1,3,10,20,30,100'
 command2 = 'switchport trunk allowed vlan 1,3,100,200,300'
 
@@ -9,3 +17,13 @@ or
 a.intersection(b)
 
 
+###########
+объясненя по строчно
+удалеяем левую часть строки
+command1.lstrip('switchport trunk allowed vlan ') 
+разбиваем строку на части по разделителю (,)
+command1.split(','))
+преобразуем строку в множества
+a=set(command1.lstrip('switchport trunk allowed vlan ').split(','))
+получаем пересения множества 
+a & b  или еще можно a.intersection(b)
